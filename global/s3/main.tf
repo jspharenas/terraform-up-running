bcaf92868c2b0684f86be0a583ba7c048989c02a
Joseph Arenas <r007833r@gmail.com>
2019-03-12 00:11:59 -0400

    Moved output variables to outputs.tf.

--- global/s3/main.tf
+++ global/s3/main.tf
@@ -46,11 +46,3 @@ resource "aws_dynamodb_table" "dynamodb_terraform-state-lock" {
     Name = "DynamoDB Terraform State Lock Table"
   }
 }
-
-output "s3_bucket_arn" {
-  value = "${aws_s3_bucket.f0084r-terraform-up-running-remote-state.arn}"
-}
-
-output "dynamodb_table_arn" {
-  value = "${aws_dynamodb_table.dynamodb_terraform-state-lock.arn}"
-}
