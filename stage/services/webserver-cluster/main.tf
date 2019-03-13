3b4e4014aa1bd2858a8c1a18a8be8df7e9f77034
Joseph Arenas <r007833r@gmail.com>
2019-03-13 13:57:16 -0400

    Re-enabled remote state lock.

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     bucket = "f0084r-terraform-up-running-remote-state"
     key = "stage/services/webserver-cluster/terraform.tfstate"
-#    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-state-lock"
     region = "us-east-1"
     encrypt = "true"
   }
