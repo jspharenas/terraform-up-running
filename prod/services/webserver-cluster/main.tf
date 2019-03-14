3272b8c70601196ebafe939b5ea628902ee88db4
Joseph Arenas <hotsoup2.0@icloud.com>
2019-03-13 23:04:41 -0400

    Created modules for data stores and webserver cluster.

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
-
+  
   cluster_name = "webservers-stage"
   db_remote_state_bucket = "f0084r-terraform-up-running-remote-state"
   db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
