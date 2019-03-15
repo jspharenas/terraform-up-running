d8a6f7f13405948f93c4ba48d37c925ea5b66bc8
Joseph Arenas <hotsoup2.0@icloud.com>
2019-03-15 15:06:01 -0400

    Broke out ELB security group rules into its own resources.

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -83,20 +83,28 @@ resource "aws_autoscaling_group" "example" {
 
 resource "aws_security_group" "elb" {
   name = "${var.cluster_name}-elb"
+}
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = "${aws_security_group.elb.id}"
+
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = "${aws_security_group.elb.id}"
+
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 
